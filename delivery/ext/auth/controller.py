462a24ae127e5fbfb606039eb21a271d53567da4
DemonLhama <iqnikolopoulos@gmail.com>
2021-05-14 09:42:39 -0300

    Controller user creation, commit in db, algorithm password

--- delivery/ext/auth/controller.py
+++ delivery/ext/auth/controller.py
@@ -3,13 +3,14 @@ from delivery.ext.auth.models import User
 from delivery.ext.db import db
 
 
-ALG = "pbkdf2:sha256"
-
+#To create a user: You will need the class User created in models and the db (SQLachemy)
+# In password use werkzeug hashing to add an algorithm with 
+# generate_password_hash and his values to hinder user password (SECURITY!)    
 
 def create_user(email:str, passwd:str, admin:bool = False) -> User:
     user = User(
         email = email, 
-        passwd = generate_password_hash(passwd, ALG),
+        passwd = generate_password_hash(passwd, method="pbkdf2:sha256", salt_length=8),
         admin = admin,
         )
     db.session.add(user)
